8e755064f625117b75f65eca806a39502080a517
ganzamick <ganzamick@gmail.com>
2025-04-09 17:05:03 +0200

    chore: adding pages

--- gmx_site/urls.py
+++ gmx_site/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import home
+from . import views
+from django.contrib import admin
+from django.urls import path
+from gmx_site.views import home, blog
+
 
 urlpatterns = [
     path('', home),
+     path('blog/', views.blog, name='blog'),
 ]
--- gmx_site/views.py
+++ gmx_site/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Hello, World! From GMX!")
 
+def home(request):
+    return render(request, 'landing.html')  
+
+def blog(request):
+    return render(request, 'blog.html')
