4d332ce2e0de0dfbcf8403839a826081d1c000d1
ganzamick <ganzamick@gmail.com>
2025-04-13 13:59:09 +0200

    chore: adding pages

--- gmx_site/urls.py
+++ gmx_site/urls.py
@@ -3,10 +3,13 @@ from .views import home
 from . import views
 from django.contrib import admin
 from django.urls import path
-from gmx_site.views import home, blog
+from gmx_site.views import home, blog, about, contact, projects
 
 
 urlpatterns = [
     path('', home),
-     path('blog/', views.blog, name='blog'),
+    path('blog/', views.blog, name='blog'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('projects/', views.projects, name='projects'),
 ]
--- gmx_site/views.py
+++ gmx_site/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'landing.html')  
+    return render(request, 'home.html')  
 
 def blog(request):
     return render(request, 'blog.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def projects(request):
+    return render(request, 'projects.html')
